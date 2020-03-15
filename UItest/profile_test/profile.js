cbe5dd69f731bdf6f7804cad34cfff69ac698dad
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2020-03-16 03:39:46 +0530

    Update profile.js

--- UItest/profile_test/profile.js
+++ UItest/profile_test/profile.js
@@ -7,7 +7,12 @@ async function prof(){
     .build()
     
     await driver.get('https://nostalgic-mccarthy-58e272.netlify.com/profile')
+var millisecondsToWait = 8000;
+setTimeout(function() {
+    // Whatever you want to do after the wait
+}, millisecondsToWait);
 
+    await driver.manage().window().maximize();
     await(await driver.findElement(By.xpath('/html/body/div/div/div/nav/div[2]/div[2]/div/div/a'))).click()
 
     await driver.getPageSource().then(function(content) 
