5ffc743ed45bf4cbfb878312ea4818835f42d473
Saiharsha Balasubramaniam <saibalsu@gmail.com>
2020-03-16 03:23:47 +0530

    Add Notifications

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -1,5 +1,58 @@
 <template>
-    
+    <div class="container">'
+        <h1 class="title is-3 has-text-centered">Notifications</h1>
+        <div class="columns is-centered">
+            <div class="column is-one-fifth">
+                
+            </div>
+            <div class="column">
+                <div class="notification is-primary is-light">
+                    <button class="delete"></button>
+                    Primar lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+
+                <div class="notification is-link is-light">
+                    <button class="delete"></button>
+                    Primar lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+
+                <div class="notification is-info is-light">
+                    <button class="delete"></button>
+                    Primar lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+
+                <div class="notification is-success is-light">
+                    <button class="delete"></button>
+                    Primar lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+
+                <div class="notification is-warning is-light">
+                    <button class="delete"></button>
+                    Primar lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+
+                <div class="notification is-danger is-light">
+                    <button class="delete"></button>
+                    Primar lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+            </div>
+            <div class="column is-one-fifth">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
